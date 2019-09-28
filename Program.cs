a489d524c4a14edc959701885efe6cbdea8a3324
ApisakAmon <54973992+ApisakAmon@users.noreply.github.com>
2019-09-28 12:39:37 +0700

    Start Use Git

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
